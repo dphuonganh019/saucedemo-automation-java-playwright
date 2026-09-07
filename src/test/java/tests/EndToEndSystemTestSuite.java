0277ea2c025a8a874f383dcaf1e9aa6a68a40cd0
Dell 7490 <dphuonganh019@gmail.com>
2026-09-07 23:19:40 +0700

    Register TC_LOGIN_02 in EndToEndSystemTestSuite

--- src/test/java/tests/EndToEndSystemTestSuite.java
+++ src/test/java/tests/EndToEndSystemTestSuite.java
@@ -6,6 +6,7 @@ import org.junit.platform.suite.api.Suite;
 @Suite
 @SelectClasses({
         TC_LOGIN_01.class,
+        TC_LOGIN_02.class,
         TC_LOGIN_10.class
 })
 public class EndToEndSystemTestSuite {
