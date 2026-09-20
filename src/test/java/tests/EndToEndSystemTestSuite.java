db3f1cea4f96cfad7887fda17de9aed5d90473c5
Dell 7490 <dphuonganh019@gmail.com>
2026-09-20 21:09:14 +0700

    Apply @ParallelSuite to EndToEndSystemTestSuite
    TC_LOGIN_01/02/10 each manage their own isolated ThreadLocal browser
    via BaseTest, so the classes have no dependency on each other and can
    run concurrently.

--- src/test/java/tests/EndToEndSystemTestSuite.java
+++ src/test/java/tests/EndToEndSystemTestSuite.java
@@ -1,9 +1,12 @@
 package tests;
 
+import config.FlowSuite;
+import config.ParallelSuite;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
+@ParallelSuite
 @SelectClasses({
         TC_LOGIN_01.class,
         TC_LOGIN_02.class,
